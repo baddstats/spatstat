b7fdc8496b95012c282d73d9ade529390091804b
Adrian Baddeley <adrian.baddeley@uwa.edu.au>
2021-02-11 11:40:09 +0800

    Straightened out NEWS file etc

--- R/First.R
+++ R/First.R
@@ -12,7 +12,14 @@
                  fields="Version")
   vs <- as.character(vs)
   putSpatstatVariable("SpatstatVersion", vs)
-  packageStartupMessage(paste("spatstat", vs))
+  nickfile <- system.file("doc", "Nickname.txt", package="spatstat")
+  ni <- scan(file=nickfile, what=character(), n=1, quiet=TRUE)
+  msg <- paste("\nspatstat", vs,
+               "     ",
+               paren(paste("nickname:", sQuote(ni))),
+               "\nFor an introduction to spatstat, type",
+               sQuote("beginner"), "\n")
+  packageStartupMessage(msg)
   return(invisible(NULL))
 }
 
